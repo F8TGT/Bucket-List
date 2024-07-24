2667529bfb0b99c1a5e241e7434813b1d18240a8
Edward <78192184+F8TGT@users.noreply.github.com>
2024-07-24 10:47:57 -0600

    -Added new functionality to add a new bucket list item.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  itemCount: number = 0;
+  btnText: string = 'Add an item';
+  goalText: string = 'My first life goal';
+  goals: string[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    if (this.goalText.trim()) {
+      this.goals.push(this.goalText);
+      this.goalText = '';
+      this.itemCount = this.goals.length;
+    }
+  }
 
 }
