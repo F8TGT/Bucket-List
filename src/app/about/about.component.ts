de097a7dc041e6c429683e39c313ef54e3c064e5
Edward <78192184+F8TGT@users.noreply.github.com>
2024-07-29 08:01:10 -0600

    -Added service file.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,19 +1,26 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router } from "@angular/router";
+import {DataService} from "../data.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private router: Router) {
+
+  goals:any;
+  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) {
     this.route.params.subscribe(res => console.log(res['id']));
   }
 
   ngOnInit(){
+    this._data.goal.subscribe(res => this.goals = res);
   }
 
   sendMeHome() {
